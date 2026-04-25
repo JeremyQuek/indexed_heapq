90ffd8d9277f55f6cf21d6da1b82b64f0a4a8f9c
Jeremy Quek <123631803+JeremyQuek@users.noreply.github.com>
2026-04-24 19:34:00 -0700

    Rename heapfix method to heapfix_max

--- core.py
+++ core.py
@@ -322,7 +322,7 @@ class maxindex_pq:
 
         # Bubble bottom down
         if delete_idx!=len(self._heap):
-            self._heapfix(bottom_key, bottom_value)
+            self._heapfix_max(bottom_key, bottom_value)
         return old_value
 
 
